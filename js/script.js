337b6ab2959133daeedfb53fb996defd26d1e329
WebGCDev <gaetano.chi@gmail.com>
2023-11-10 11:47:35 +0100

    feat:Fix con stampa console anche caratteri pari.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 //Descrizione esercizio: L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 
 const word1 = prompt('Inserisci la prima parola');
+console.log('La prima parola è: ', word1);
 const word2 = prompt('Inserisci la seconda parola');
+console.log('La seconda parola è: ', word2);
 
-if (word1.length > word2.length) {
-  console.log(word1);
-  console.log(word2);
+//Correzzione con stampa in console con i 3 risultati, parola più lunga parola più corta, possono avere anche caratteri uguali che non avevo valutato nella prima versione
+
+if (word1.length < word2.length) {
+  console.log('La parola più corta è:', word1);
+  console.log('La parola più lunga è:', word2);
+} else if (word1.length > word2.length) {
+  console.log('La parola più corta è:', word2);
+  console.log('La parola più lunga è:', word1);
 } else {
-  console.log(word2);
-  console.log(word1);
+  console.log('I caratteri delle parole sono uguali.');
 }
